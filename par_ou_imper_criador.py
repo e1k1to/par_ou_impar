ca98c7acedd920efdbe0c269e0c22625c7d55600
Lucca Eiki <84162201+e1k1to@users.noreply.github.com>
2021-05-13 22:19:28 -0300

    Update par_ou_imper_criador.py

--- par_ou_imper_criador.py
+++ par_ou_imper_criador.py
@@ -2,8 +2,9 @@ f = open("batota.txt","w+")
 
 for i in range(10001):
     if i % 2 == 0:
-        oddeven = "even"
+        oddeven = "par"
     else:
-        oddeven = "odd"
+        oddeven = "impar"
 
-    f.write("if num == {}:\n    print('Your number is {}')\n".format(str(i),oddeven))
+    f.write("if num == {}:\n    print('Seu número é {}')\n".format(str(i),oddeven))
+    f.write("if num > 10000:\n    print('Não sei esse número, desculpa :)!!!'")
